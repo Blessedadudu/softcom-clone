545bdaec1743869e5d9338fbef0cf72be10361e5
Blessedadudu <adududuke@gmail.com>
2020-05-29 17:30:35 +0100

    update

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -9,6 +9,28 @@ const Body = () => {
                 <p>Following our work over the years, we have identified four key areas that would enable people and businesses thrive in Africa. Namely: Identity, Learning, Payment and Data.</p>
             </div>
             <div className='bodycardGrid'>
+                <div className='arrow'><span> &#8594; </span></div>
+                <div className='arrow'><span> &#8594; </span></div>
+                <div className='cards1'>Card 1</div>
+                <div className='cards2'>Card 1</div>
+                <div className='cards3'>Card 1</div>
+                <div className='cards4'>Card 1</div>
+            </div>
+            <div className='bodyText'>
+                <h2>Our Focus</h2>
+                <p>Following our work over the years, we have identified four key areas that would enable people and businesses thrive in Africa. Namely: Identity, Learning, Payment and Data.</p>
+            </div>
+            <div className='bodycardGrid'>
+                <div className='cards1'>Card 1 </div>
+                <div className='cards2'>Card 1</div>
+                <div className='cards3'>Card 1</div>
+                <div className='cards4'>Card 1</div>
+            </div>
+            <div className='bodyText'>
+                <h2>Our Focus</h2>
+                <p>Following our work over the years, we have identified four key areas that would enable people and businesses thrive in Africa. Namely: Identity, Learning, Payment and Data.</p>
+            </div>
+            <div className='bodycardGrid'>
                 <div className='cards1'>Card 1</div>
                 <div className='cards2'>Card 1</div>
                 <div className='cards3'>Card 1</div>
