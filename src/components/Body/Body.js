1ef2807e4f927d5cde03a96f31b0464e8b0fbd1d
Blessedadudu <adududuke@gmail.com>
2020-05-31 17:20:08 +0100

    update on card

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -10,16 +10,43 @@ const Body = () => {
                 <p>Following our work over the years, we have identified four key areas that would enable people and businesses thrive in Africa. Namely: Identity, Learning, Payment and Data.</p>
             </div>
             <div className='bodycardGrid'>
-                <div className='arrow'><span> &#8594; </span></div>
-                <div className='cards1'>
-                    <img src={passId} alt='pass ID'/>
-                    <h3>Identity</h3>
-                    <h4>Enroll & Verify Anyone</h4>
-                    <p>We believe that no society can truly grow her community without first identifying the people that belong to the community. Identity is the bedrock to planning, financial inclusion and targeting.</p>
-                </div>
-                <div className='cards2'>Card 1</div>
-                <div className='cards3'>Card 1</div>
-                <div className='cards4'>Card 1</div>
+                <>
+                    <div className='arrow'><span> &#8594; </span></div>
+                    <div className='cards1'>
+                        <img src={passId} alt='pass ID'/>
+                        <h3>Identity</h3>
+                        <h4>Enroll & Verify Anyone</h4>
+                        <p>We believe that no society can truly grow her community without first identifying the people that belong to the community. Identity is the bedrock to planning, financial inclusion and targeting.</p>
+                    </div>
+                </>
+                <>
+                    
+                    <div className='cards1'>
+                        <div className='arrow'><span> &#8594; </span></div>
+                        <img src={passId} alt='pass ID'/>
+                        <h3>Identity</h3>
+                        <h4>Enroll & Verify Anyone</h4>
+                        <p>We believe that no society can truly grow her community without first identifying the people that belong to the community. Identity is the bedrock to planning, financial inclusion and targeting.</p>
+                    </div>   
+                </>
+                <>  
+                    <div className='cards1'>
+                        <div className='arrow'><span> &#8594; </span></div>
+                        <img src={passId} alt='pass ID'/>
+                        <h3>Identity</h3>
+                        <h4>Enroll & Verify Anyone</h4>
+                        <p>We believe that no society can truly grow her community without first identifying the people that belong to the community. Identity is the bedrock to planning, financial inclusion and targeting.</p>
+                    </div>   
+                </>
+                <>
+                    <div className='cards1'>
+                        <div className='arrow'><span> &#8594; </span></div>
+                        <img src={passId} alt='pass ID'/>
+                        <h3>Identity</h3>
+                        <h4>Enroll & Verify Anyone</h4>
+                        <p>We believe that no society can truly grow her community without first identifying the people that belong to the community. Identity is the bedrock to planning, financial inclusion and targeting.</p>
+                    </div>   
+                </>
             </div>
             <div className='bodyText'>
                 <h2>Our Focus</h2>
