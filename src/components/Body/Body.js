51322d9a549258ca700ee5e3fb3dcec0cf37e8ff
Blessedadudu <adududuke@gmail.com>
2020-06-09 10:55:07 +0100

    update

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -144,7 +144,15 @@ const Body = () => {
             <div className='section-5'>
                 <div className='section-5__image-container'>
                     <img src='https://softcom.ng/uploads/home/eyowoweb-banner.jpg' alt='lightskin girl' className='section-5__image-container__img'/>
+                    <div className='image-overlay'></div>
                 </div>
+                <div className='section-5__payment'>
+                    <h1>Make Payments. <br/>However, Whenever</h1>
+                    <p>Send, receive and pay bills with just your phone number.<br/>
+                        Carry-out transactions with or without a bank account, whether online or offline.</p>
+                    <button>Experience Eyowo <span>&raquo;</span></button>
+                </div>
+                
                 
             </div>
         </body>
