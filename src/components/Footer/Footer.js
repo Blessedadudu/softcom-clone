826197f43dbad77a3456b65a59354010b9e32803
Blessedadudu <adududuke@gmail.com>
2020-06-09 10:19:39 +0100

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -33,7 +33,14 @@ const Footer = () => {
                     <h4>More...</h4>
                     <p>Privacy Policy</p>
                     <p>Blog</p>
-                    <p>DevX Blog<span>X</span></p>
+                    <p>DevX Blog
+                        <span> <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon xlink">
+                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                <polyline points="15 3 21 3 21 9"></polyline>
+                                <line x1="10" y1="14" x2="21" y2="3"></line>
+                            </svg>
+                        </span>
+                    </p>
                 </div>
 
                 <div className='footer__grid-4'>
@@ -45,7 +52,7 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <p>Copyright 2020 Softcom Ltd.</p>
+            <p className='footer__copyright'>Copyright 2020 Softcom Ltd.</p>
         </footer>
     )
 }
