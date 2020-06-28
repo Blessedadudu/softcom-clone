5c901cc7ae066bf5e04bfa25b42aaa404bf8d915
Blessedadudu <adududuke@gmail.com>
2020-06-28 10:35:37 +0100

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,6 +43,7 @@ const Header = () => {
                             <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                             <circle cx="12" cy="10" r="3"></circle>
                         </svg>
+                        <span className='svgDivider'></span>
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
                                 <rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect>
